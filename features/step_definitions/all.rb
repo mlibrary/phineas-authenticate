db684c5e1802be4e53e8902963b9ff0797899262
Matthew Alexander LaChance <mattlach@umich.edu>
2023-10-16 08:25:00 -0400

    Get feature tests working with dex

--- features/step_definitions/all.rb
+++ features/step_definitions/all.rb
@@ -1,27 +1,39 @@
 Given("I am an authorized technician") do
-  pending # Write code here that turns the phrase above into concrete actions
-end
-
-Given("I open this application") do
-  pending # Write code here that turns the phrase above into concrete actions
+  @username = ENV["MOCK_USERNAME"]
+  @password = ENV["MOCK_PASSWORD"]
 end
 
 When("I request a new token") do
-  pending # Write code here that turns the phrase above into concrete actions
+  visit("/")
+  click_button("Log in")
 end
 
 Then("I am directed to an upstream identity provider") do
-  pending # Write code here that turns the phrase above into concrete actions
+  expect(page.current_url).to match(/^#{Regexp.escape(ENV["DEX_URL"])}/)
 end
 
-Given("I have been directed to an upstream identity provider") do
-  pending # Write code here that turns the phrase above into concrete actions
+Given("I have already requested a new token") do
+  visit("/")
+  click_button("Log in")
 end
 
 When("I prove my identity") do
-  pending # Write code here that turns the phrase above into concrete actions
+  expect(page).not_to have_text(/invalid/i)
+  find_field(id: "login").set(@username)
+  fill_in("Password", with: @password)
+  click_button("Login")
 end
 
 Then("I receive a token that will work for kubernetes") do
-  pending # Write code here that turns the phrase above into concrete actions
+  expect(find("pre").text).to match(/--auth-provider-arg=id-token=[-_0-9A-Za-z]+\.[-_0-9A-Za-z]+\.[-_0-9A-Za-z]+/)
+end
+
+Given("I am not an authorized technician") do
+  @username = "malefactor@realcanadianpharmacy.ru"
+  @password = "admin"
+end
+
+Then("I do not receive a token") do
+  expect(page.current_url).to match(/^#{Regexp.escape(ENV["DEX_URL"])}/)
+  expect(page).to have_text(/invalid/i)
 end
--- features/support/env.rb
+++ features/support/env.rb
@@ -1,7 +1,8 @@
 require "capybara/cucumber"
+require "selenium-webdriver"
 
-# This requires a web driver to be installed, and we went with gecko:
-# https://github.com/mozilla/geckodriver/releases
 Capybara.default_driver = :selenium_headless
 Capybara.app_host = ENV["APP_URL"]
 Capybara.enable_aria_label = true
+
+Selenium::WebDriver.logger.level = :error
