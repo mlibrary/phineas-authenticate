270405fdd7f2d50be74f762e65ecd08b7f1366bd
Matthew Alexander LaChance <mattlach@umich.edu>
2023-10-16 16:32:00 -0400

    Refactor steps to drive the browser

--- features/step_definitions/all.rb
+++ features/step_definitions/all.rb
@@ -3,37 +3,37 @@ Given("I am an authorized technician") do
   @password = ENV["MOCK_PASSWORD"]
 end
 
-When("I request a new token") do
-  visit("/")
-  click_button("Log in")
-end
-
-Then("I am directed to an upstream identity provider") do
-  expect(page.current_url).to match(/^#{Regexp.escape(ENV["DEX_URL"])}/)
-end
-
-Given("I have already requested a new token") do
-  visit("/")
-  click_button("Log in")
-end
-
-When("I prove my identity") do
-  expect(page).not_to have_text(/invalid/i)
-  find_field(id: "login").set(@username)
-  fill_in("Password", with: @password)
-  click_button("Login")
-end
-
-Then("I receive a token that will work for kubernetes") do
-  expect(find("pre").text).to match(/--auth-provider-arg=id-token=[-_0-9A-Za-z]+\.[-_0-9A-Za-z]+\.[-_0-9A-Za-z]+/)
-end
-
 Given("I am not an authorized technician") do
   @username = "malefactor@realcanadianpharmacy.ru"
   @password = "admin"
 end
 
+Given("I have already requested a new token") do
+  open_login_app
+  request_a_new_token
+end
+
+When("I request a new token") do
+  open_login_app
+  request_a_new_token
+end
+
+When("I prove my identity") do
+  expect_no_error_messages
+  enter_username(@username)
+  enter_password(@password)
+  submit_login_form
+end
+
+Then("I am directed to an upstream identity provider") do
+  expect_url_is_upstream_idp
+end
+
+Then("I receive a token that will work for kubernetes") do
+  expect_valid_jwt
+end
+
 Then("I do not receive a token") do
-  expect(page.current_url).to match(/^#{Regexp.escape(ENV["DEX_URL"])}/)
-  expect(page).to have_text(/invalid/i)
+  expect_url_is_upstream_idp
+  expect_an_error_message
 end
